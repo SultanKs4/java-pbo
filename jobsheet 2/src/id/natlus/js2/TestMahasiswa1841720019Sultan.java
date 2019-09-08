2bdd94044e043dd1bbe04263b1188dd0bbba7e84
SultanKs4 <sultangendut@gmail.com>
2019-09-08 12:37:03 +0700

    init Jobsheet 2

---
+++ jobsheet 2/src/id/natlus/js2/TestMahasiswa1841720019Sultan.java
@@ -0,0 +1,9 @@
+package id.natlus.js2;
+
+public class TestMahasiswa1841720019Sultan {
+    public static void main(String[] args) {
+        Mahasiswa1841720019Sultan mhs1 = new Mahasiswa1841720019Sultan(101, "Lestari",
+                "Jl. Vinolia No 1A", "1A");
+        mhs1.tampilBiodataSultan();
+    }
+}
